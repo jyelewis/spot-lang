24b286b13d885060a2c205a426209298ec9eabdd
Jye Lewis <jye@jyelewis.com>
2025-09-28 11:12:55 +1000

    Tests

---
+++ packages/spot-engine/src/examples.test.ts
@@ -0,0 +1,19 @@
+import path from 'node:path';
+import { executeCodeFile } from './index';
+import fs from 'node:fs/promises';
+
+const examplesDir = `${__dirname}/../../../docs/code_examples`;
+
+describe('Test against examples', () => {
+  const runsExample = async (exampleName: string) => {
+    const codeFilePath = path.join(examplesDir, `${exampleName}.spot`);
+    const expectedOutputFilePath = path.join(examplesDir, `${exampleName}_output.txt`);
+
+    const output = await executeCodeFile(codeFilePath);
+    const expectedOutput = await fs.readFile(expectedOutputFilePath, 'utf-8');
+
+    expect(output).toEqual(expectedOutput);
+  };
+
+  it(`Runs example '0_print_hello.spot`, () => runsExample('0_print_hello'));
+});
