a14b83eeded8bf575396bd025651c08c358eabd3
Jye Lewis <jye@jyelewis.com>
2025-10-06 10:49:20 +1100

    [CC] Link CLI

--- src/spot-engine/index.ts
+++ src/spot-engine/index.ts
@@ -1,24 +1,10 @@
+#!/usr/bin/env node
 import fs from 'node:fs/promises';
 import { tokeniseCode } from './1_tokeniser/tokeniser';
 import { parseCodeTokens } from './2_parser/parser';
 import { compileASTToApplication } from './3_compiler/compiler';
 import { executeApplication } from './4_vm/vm';
 import { program } from 'commander';
-
-async function main() {
-  const inputFile = process.argv[2];
-  if (!inputFile) {
-    console.error('Usage: spot <input-file.spot>');
-    process.exit(1);
-  }
-
-  const code = await fs.readFile(inputFile, 'utf-8');
-
-  const tokens = tokeniseCode(code);
-  const ast = parseCodeTokens(tokens);
-  const byteCodeApplication = compileASTToApplication(ast);
-  await executeApplication(byteCodeApplication);
-}
 
 program.name('spot').description('The spot programming langauge').version('0.0.1');
 
